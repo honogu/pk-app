6a41a2e9ec1ccbd979eae1f48e2b0a5fa5a196b6
Holger Nõgu <honogu@ttu.ee>
2024-02-04 18:42:31 +0200

    Change manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.png'],
+    manifest: {
+      name: 'Praktikakohvik',
+      short_name: 'Praktikakohvik',
+      description: 'Praktikakohvik',
+      theme_color: '#000000',
+      background_color: '#ffffff',
+      display: 'standalone',
+      start_url:"/",
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes:'192x192',
+          type:'image/png'
+        },
+        {
+          src:'pwa-512x512.png',
+          sizes:'512x512',
+          type:'image/png'
+        }
+      ]
+    }
+  })],
 })
